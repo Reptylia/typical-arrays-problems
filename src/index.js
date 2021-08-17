830de4004dc2ddbf7f181f88e94ef6efac4cdaa4
Reptylia <repty.alliance@gmail.com>
2021-08-17 20:51:28 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+  let minNum = array[0];
+  for(let i = 0;i < array.length;i++) {
+    if(array[i] < minNum) {
+      minNum = array[i];
+    }
+  }
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+  let minNum = array[0];
+  for(let i = 0;i < array.length;i++) {
+    if(array[i] > minNum) {
+      minNum = array[i];
+    }
+  }
+  return minNum;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if(arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+  let minNum = 0;
+  for(let i = 0;i < array.length;i++) {
+    
+      minNum = minNum + array[i];
+    
+  }
+  return minNum / array.length;
+  
 }
